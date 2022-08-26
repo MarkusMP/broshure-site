44fdefa62fa55bf1121a3e85332d3fd793f2739d
Markus Persson <markusperssonbiz@gmail.com>
2022-08-26 23:01:41 +0200

    v1.0.1

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import Layout from "../components/Layout";
 import { queryFooter, queryHeader, queryPolicy } from "../data/queries";
 import { getClient } from "../lib/sanity.server";
-import { PortableText } from "@portabletext/react";
+import PortableText from "react-portable-text";
+
+const components = {
+  types: {
+    code: (props) => (
+      <pre data-language={props.node.language}>
+        <code>{props.node.code}</code>
+      </pre>
+    ),
+  },
+};
 
 const contact = ({ footer, header, policy }) => {
   return (
     <Layout title="Manage | Landing Page" header={header} footer={footer}>
-      <section className="container mx-auto max-w-2xl py-12">
+      <section className="container mx-auto max-w-2xl py-20">
         <h1 className="mb-4 text-4xl tracking-tight text-center font-bold">
           Privacy & Policy
         </h1>
         <p className="mb-4 text-center">Last updated on August 26, 2022</p>
-        <PortableText value={policy.policy} />
+        {policy.policy && <PortableText content={policy.policy} />}
       </section>
     </Layout>
   );
